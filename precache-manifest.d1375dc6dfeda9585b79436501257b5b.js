7819936c322f4c43fbd92d761f5affe86d805d97
jesusvazquez <jesusvaz@gmail.com>
2020-07-18 13:33:21 -0600

    Updates

---
+++ precache-manifest.d1375dc6dfeda9585b79436501257b5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bbd31c7574ea061517105097ee12c6b",
+    "url": "/ingles/index.html"
+  },
+  {
+    "revision": "c6a62a6ec78285ceb504",
+    "url": "/ingles/static/css/main.5a0fdb76.chunk.css"
+  },
+  {
+    "revision": "64a78af4325a1ccac658",
+    "url": "/ingles/static/js/2.9472457c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ingles/static/js/2.9472457c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6a62a6ec78285ceb504",
+    "url": "/ingles/static/js/main.eb5b8820.chunk.js"
+  },
+  {
+    "revision": "26d37bbedd4332a6fa3e",
+    "url": "/ingles/static/js/runtime-main.eed79ad0.js"
+  }
+]);
